8e77736ffc9a8f45435d2e16f0361588cc6c0388
Sachin Gupta <guptasachin7698@gmail.com>
2025-06-27 02:34:33 +0530

    Initial commit

---
+++ JavaScript/app.js
@@ -0,0 +1,95 @@
+// console.log("Hello World");
+// console.log(12);
+
+// let a=10;
+// let b=10;
+// // console.log("Sum is",a+b,"answer");
+// // let output = "Sum is" + (a+b) + "answer";
+// let output = `sum is : ${a+b} answer`;
+// console.log(output);
+
+
+// let color = "red";
+// if(color=="red"){
+//     console.log("Stop the car");
+// }
+// if(color=="green"){
+//     console.log("go the car");
+// }
+// if(color=="yellow"){
+//     console.log("wait");
+// }
+
+// let str="codeing";
+// if( str[0] == "c" && str.length>3){
+//     console.log("You are good string")
+
+// } else{
+//     console.log("You are not good string");
+// }
+
+// let string = "";
+// if(string){
+//     console.log("String is not empty");
+// } else{
+//     console.log("String is empty");
+// }
+
+// let num=10;
+// if(num){
+//     console.log("num is not equal to 0");
+// } else{
+//     console.log("num is equal to 0");
+// }
+
+// let day=5;
+//  switch(day){
+//     case 1 : 
+//     console.log("Monday");
+//     break;
+//     case 2 : 
+//     console.log("tuesday");
+//     break;
+//     case 3 : 
+//     console.log("wednesday");
+//     break;
+//     case 4 : 
+//     console.log("thursday");
+//     break;
+//     case 5 : 
+//     console.log("friday");
+//     break;
+//     case 6 : 
+//     console.log("saturday");
+//     break;
+//     case 7 : 
+//     console.log("sunday");
+//     break;
+//     default:
+//         console.log("Good luck");
+
+//  }
+
+// alert("something is wrong!");
+
+// console.log("hello");
+// console.error("wrong message");
+// console.warn("warn");
+
+// prompt("Enter your name");
+
+// let num = 577;
+// if(num%10==0){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+// }
+
+let name = prompt("enter your name");
+let age= prompt("enter your age");
+// console.log( age);
+// console.log("name");
+alert(` ${name} is ${age} years old`);
+
+console.log(age);
+console.log(name);
