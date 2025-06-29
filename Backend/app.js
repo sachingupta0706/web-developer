a12fc783c86afa40216f93eb99b6e74e2c674da4
Sachin Gupta <guptasachin7698@gmail.com>
2025-06-29 11:43:27 +0530

    changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,20 @@
-let n=5;
-for(n=0;n<=5;n++){
-    console.log(n);
-}
+// let n=5;
+// for(n=0;n<=5;n++){
+//     console.log(n);
+// }
+
+// console.log(process.argv);
+
+// const someValue = require("./math");
+// console.log(someValue);
+
+// const info = require('./fruits');
+// const info = require('./apple');
+// console.log(info);
+// const info = require('./index_4.js');
+// console.log(info);
+
+// export const sum = (a,b) => a+b;
+
+import {sum} from "./math.js";
+console.log(sum(1,2));
---
+++ Backend/math.js
@@ -0,0 +1,2 @@
+// module.exports=123;
+export const sum = (a,b) => a+b;
