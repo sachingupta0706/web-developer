6346b95d5ccabbaa99e269874b1adc934c299d0a
Sachin Gupta <guptasachin7698@gmail.com>
2025-07-03 15:28:57 +0530

    changes

---
+++ JavaScript/PART 9 - DOM - IMP/app.js
@@ -0,0 +1,10 @@
+// --------- query selectors ---------
+
+console.dir(document.querySelector("h1"));
+console.dir(document.querySelector("#description"));
+console.dir(document.querySelector(".oldImg"));
+
+// -------- in query selectors we need all ---------------
+
+console.dir(document.querySelectorAll("div a"));
+
